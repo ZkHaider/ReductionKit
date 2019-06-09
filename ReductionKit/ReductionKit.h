85f0d0a83b30776e585197ce779338eeb85f757f
Haider Khan <hkhan@datalofts.com>
2019-06-09 13:22:05 -0700

    Initial Commit

---
+++ ReductionKit/ReductionKit.h
@@ -0,0 +1,19 @@
+//
+//  ReductionKit.h
+//  ReductionKit
+//
+//  Created by Haider Khan on 6/9/19.
+//  Copyright © 2019 zkhaider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReductionKit.
+FOUNDATION_EXPORT double ReductionKitVersionNumber;
+
+//! Project version string for ReductionKit.
+FOUNDATION_EXPORT const unsigned char ReductionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReductionKit/PublicHeader.h>
+
+
